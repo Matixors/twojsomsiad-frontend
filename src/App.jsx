525203e8b17b62c21da779b1e56c3f5db9dc4f93
bart016 <bart.turczynski@gmail.com>
2022-10-09 19:52:19 +0200

    adver list 1.0
    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import HelloWorld from "./components/hello-world/hello-world.component"
+import List from "./components/adverts-view/advert-list.component"
+import axios from 'axios';
+import React, { useState, useRef, useEffect } from 'react';
+import * as styles from './styles/App.module.scss';
 
 export default function App() {
+  const [data, setData] = useState([]);
+
+
+  useEffect(() => {
+      axios.get('https://jsonplaceholder.typicode.com/users').then(data => {
+          setData(data.data);
+      })
+  }, []);
+
   return (
     <>
       <HelloWorld />
+      {data.map((user, index) => {
+                return (
+                    <List key={index} data={user}/>
+                    )
+                })}
     </>
   )
 }
