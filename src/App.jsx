dbd10382d32ff29e375dc377c667c02287ba1b5b
bart016 <bart.turczynski@gmail.com>
2022-10-10 22:04:18 +0200

    super avatars and smaller height

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import List from "./components/adverts-view/advert-list.component"
 import axios from 'axios';
 import React, { useState, useRef, useEffect } from 'react';
 import * as styles from './styles/App.module.scss';
-
+import { createAvatar } from '@dicebear/avatars';
+import * as style from '@dicebear/pixel-art';
 export default function App() {
   const [data, setData] = useState([]);
 
@@ -18,8 +19,15 @@ export default function App() {
     <>
       <HelloWorld />
       {data.map((user, index) => {
+        let rand = Math.random()
+        rand = rand.toString()
+        let svg = createAvatar(style, {
+          seed: rand,
+          scale: 30,
+          size: 100,
+        });
                 return (
-                    <List key={index} data={user}/>
+                    <List key={index} data={user} svg={svg} />
                     )
                 })}
     </>
