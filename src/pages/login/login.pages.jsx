dd522513967b9875cbf062764ff404906cc6077d
OreoPL-studio <81591050+OreoPL-studio@users.noreply.github.com>
2022-10-15 22:42:01 +0200

    Login auth

--- src/pages/login/login.pages.jsx
+++ src/pages/login/login.pages.jsx
@@ -12,6 +12,7 @@ import Container from '@mui/material/Container';
 import { NavLink } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import * as styles from '../../styles/Login.module.scss';
+import axios from "axios";
 
 function Copyright(props) {
   return (
@@ -29,81 +30,89 @@ function Copyright(props) {
 const theme = createTheme();
 
 export default function Login() {
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      const data = new FormData(event.currentTarget);
-      console.log({
-        email: data.get('email'),
-        password: data.get('password'),
-      });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const options = {
+      method: 'POST',
+      url: 'https://twojsomsiad-backend.onrender.com/auth/login',
+      headers: { 'Content-Type': 'application/json' },
+      data: {email: data.get('email'),	password: data.get('password')}
     };
-  
-    return (
-      <ThemeProvider theme={theme}>
-        <Container component="main" maxWidth="xs">
-          <CssBaseline />
-          <Box
-            sx={{
-              marginTop: 8,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: 'lightgreen' }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Logowanie
-            </Typography>
-            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Adres email"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Hasło"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Zaloguj się
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <NavLink to="/">
-                    <Link variant="body2">
-                      Powrót do strony głównej
-                    </Link>
-                  </NavLink>
-                </Grid>
-                <Grid item>
-                  <NavLink to="/register">
-                    <Link variant="body2">
-                      Masz już konto? Zarejestruj się
-                    </Link>
-                  </NavLink>
-                </Grid>
+
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'lightgreen' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Logowanie
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Adres email"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Hasło"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Zaloguj się
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <NavLink to="/">
+                  <Link variant="body2">
+                    Powrót do strony głównej
+                  </Link>
+                </NavLink>
               </Grid>
-            </Box>
+              <Grid item>
+                <NavLink to="/register">
+                  <Link variant="body2">
+                    Masz już konto? Zarejestruj się
+                  </Link>
+                </NavLink>
+              </Grid>
+            </Grid>
           </Box>
-        </Container>
-      </ThemeProvider>
-    );
-  }
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
